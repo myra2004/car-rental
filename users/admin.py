4ff0775e279749eacb318d4dccff68df8223161d
myra2004 <amirovamuqaddas@gmail.com>
2025-05-03 14:36:52 +0500

    added models

---
+++ users/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("id",  "email", 'username', 'first_name', 'last_name', 'is_staff')
+    list_display_links = ("id",'username', "email", 'first_name', 'last_name')
+    search_fields = ('id', "email", 'username', 'first_name', 'last_name')
+
+    fieldsets = (
+        ('User', {'fields': ('username', 'password', 'email')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'avatar', 'profession', 'bio')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+
+# from common.models import BaseModel
+
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    profession = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
